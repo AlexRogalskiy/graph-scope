b47ada7ab6f865f1be6277eb99a56b1b57d8aca5
Stephen Karger <stephen.karger@gmail.com>
2018-10-24 22:52:18 -0400

    Extract graphs.css

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,11 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+
+    // process imports in app.css, reference: https://ember-cli.com/user-guide/#css
+    minifyCSS: {
+        options: { processImport: true }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
